b54dbd4de8f2d289670fdf11cf18e70b0ff092d7
evgeniylitvinov <evgeniy.litvinov88@gmail.com>
2015-02-24 20:36:47 +0200

    Login Activity

--- app/src/main/java/com/rakovpublic/shoestore/LoginActivity.java
+++ app/src/main/java/com/rakovpublic/shoestore/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 
@@ -17,6 +18,12 @@ public class LoginActivity extends Activity {
         Button loginBut=(Button)findViewById(R.id.button_login);
         Button registerBut=(Button)findViewById(R.id.button_registration);
         Button forgotpassBut=(Button)findViewById(R.id.button_forgotpassword);
+        registerBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
 }
